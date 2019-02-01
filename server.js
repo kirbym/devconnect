9555786769232649a23b3f0f862051189c4827bb
Michael Kirby <kirbyma729@gmail.com>
2019-01-31 21:00:24 -0500

    user model

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
@@ -23,6 +20,8 @@ mongoose
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+
+app.get('/', (req, res) => res.json({ message: 'host up and running' }));
 
 const port = process.env.PORT || 5000;
 
