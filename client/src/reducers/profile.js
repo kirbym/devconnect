394273e002b4d2d192972fb69c397173e69d8d55
Michael Kirby <kirbyma729@gmail.com>
2019-06-05 11:07:37 -0400

    create profile, education, experience

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,9 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from '../actions/types';
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+  UPDATE_PROFILE
+} from '../actions/types';
 
 const initialState = {
   profile: null,
@@ -11,6 +16,7 @@ const initialState = {
 export default function(state = initialState, action) {
   switch (action.type) {
     case GET_PROFILE:
+    case UPDATE_PROFILE:
       return {
         ...state,
         profile: action.payload,
