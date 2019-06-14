0232f75a4f7a8dfc823f1672e572836be62006fa
Michael Kirby <kirbyma729@gmail.com>
2019-06-14 00:03:31 -0400

    tweek dashboard display when no edu/exp

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -31,17 +31,21 @@ const Education = ({ education, deleteEducation }) => {
   return (
     <Fragment>
       <h2 className="my-2">Education Credentials</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>School</th>
-            <th className="hide-sm">Degree</th>
-            <th className="hide-sm">Years</th>
-            <th />
-          </tr>
-        </thead>
-        <tbody>{educations}</tbody>
-      </table>
+      {education.length === 0 ? (
+        <Fragment>No education history added yet.</Fragment>
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>School</th>
+              <th className="hide-sm">Degree</th>
+              <th className="hide-sm">Years</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>{educations}</tbody>
+        </table>
+      )}
     </Fragment>
   );
 };
--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -31,17 +31,21 @@ const Experience = ({ experience, deleteExperience }) => {
   return (
     <Fragment>
       <h2 className="my-2">Experience Credentials</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Company</th>
-            <th className="hide-sm">Title</th>
-            <th className="hide-sm">Years</th>
-            <th />
-          </tr>
-        </thead>
-        <tbody>{experiences}</tbody>
-      </table>
+      {experience.length === 0 ? (
+        <Fragment>No work history added yet.</Fragment>
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Company</th>
+              <th className="hide-sm">Title</th>
+              <th className="hide-sm">Years</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>{experiences}</tbody>
+        </table>
+      )}
     </Fragment>
   );
 };
