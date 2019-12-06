a1c6049f54de89eec447af23ce2b3eda7ef84f97
Michael Kirby <kirbyma729@gmail.com>
2019-12-05 21:13:01 -0500

    revert test change

--- server.js
+++ server.js
@@ -27,6 +27,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 5005;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
