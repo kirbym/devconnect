272113ed032b6b76a81361e31c01a3319371972c
Michael Kirby <kirbyma729@gmail.com>
2020-11-23 16:50:32 -0500

    profile bug: If a guest user browses a dev profile and then registers, the browsed users profile data is still in the 'profile' state and the newly registered user then sees and can edit the users info

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -4,50 +4,51 @@ import {
   CLEAR_PROFILE,
   UPDATE_PROFILE,
   GET_PROFILES,
-  GET_REPOS
-} from '../actions/types';
+  GET_REPOS,
+} from "../actions/types";
 
 const initialState = {
   profile: null,
   profiles: [],
   repos: [],
   loading: true,
-  error: {}
+  error: {},
 };
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   switch (action.type) {
     case GET_PROFILE:
     case UPDATE_PROFILE:
       return {
         ...state,
         profile: action.payload,
-        loading: false
+        loading: false,
       };
     case GET_PROFILES:
       return {
         ...state,
         profiles: action.payload,
-        loading: false
+        loading: false,
       };
     case PROFILE_ERROR:
       return {
         ...state,
         error: action.payload,
-        loading: false
+        profile: null,
+        loading: false,
       };
     case CLEAR_PROFILE:
       return {
         ...state,
         profile: null,
         repos: [],
-        loading: false
+        loading: false,
       };
     case GET_REPOS:
       return {
         ...state,
         repos: action.payload,
-        loading: false
+        loading: false,
       };
     default:
       return state;
